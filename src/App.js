5e372cd135579006f3554d9a584c4f42b1dcadde
Alejandro Zambrano <alejozd79@gmail.com>
2024-05-21 10:21:45 -0500

    cambios apariencia contacto

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import Home from "./Home"; // Importa tu componente Home
 import Clientes from "./Clientes"; // Importa tu componente Clientes
 import Contacto from "./Contacto"; // Importa tu componente Contacto
 import Productos from "./Productos"; // Importa tu componente Productos
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primereact/resources/primereact.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 function App() {
   return (
