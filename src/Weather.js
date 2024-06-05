5a5b02ec898a18ecea445b203d526fe997c5ba4a
Alejandro Zambrano <alejozd79@gmail.com>
2024-06-05 15:28:33 -0500

    con mas informacion

--- src/Weather.js
+++ src/Weather.js
@@ -4,6 +4,8 @@ import { Card } from "primereact/card";
 import { Button } from "primereact/button";
 import { InputText } from "primereact/inputtext";
 import { Skeleton } from "primereact/skeleton";
+import { Divider } from "primereact/divider";
+import { Panel } from "primereact/panel";
 
 const Weather = () => {
   const [city, setCity] = useState("");
@@ -63,21 +65,16 @@ const Weather = () => {
     <div className="p-d-flex p-jc-center p-ai-center p-mt-5">
       <Card
         title="Pronóstico del Tiempo"
-        style={{ width: "25em", textAlign: "center" }}
+        style={{ width: "50em", textAlign: "center" }}
       >
         <div className="p-inputgroup p-mb-5 p-d-flex p-jc-center">
           <InputText
             value={city}
             onChange={(e) => setCity(e.target.value)}
             placeholder="Ingrese el nombre de la ciudad"
-            style={{ marginRight: "0.5em", width: "80%" }}
+            style={{ marginRight: "0.5em", width: "70%" }}
           />
-          <Button
-            label="Buscar"
-            icon="pi pi-search"
-            onClick={fetchCityId}
-            severity="warning"
-          />
+          <Button label="Buscar" icon="pi pi-search" onClick={fetchCityId} />
         </div>
         {loading && (
           <div className="p-d-flex p-flex-column p-ai-center">
@@ -90,64 +87,84 @@ const Weather = () => {
         )}
         {error && <p style={{ color: "red" }}>{error}</p>}
         {weatherData && (
-          <div className="p-d-flex p-flex-column p-ai-center">
-            <img
-              src={weatherIconUrl(weatherData.WeatherIcon)}
-              alt={weatherData.WeatherText}
-            />
-            <p>
-              <strong>Condición:</strong> {weatherData.WeatherText}
-            </p>
-            <p>
-              <strong>Temperatura:</strong>{" "}
-              {weatherData.Temperature.Metric.Value}°
-              {weatherData.Temperature.Metric.Unit}
-            </p>
-            <p>
-              <strong>Temperatura Real:</strong>{" "}
-              {weatherData.RealFeelTemperature.Metric.Value}°
-              {weatherData.RealFeelTemperature.Metric.Unit} (
-              {weatherData.RealFeelTemperature.Metric.Phrase})
-            </p>
-            <p>
-              <strong>Humedad Relativa:</strong> {weatherData.RelativeHumidity}%
-            </p>
-            <p>
-              <strong>Hora de Observación:</strong>{" "}
-              {new Date(weatherData.LocalObservationDateTime).toLocaleString()}
-            </p>
-            <p>
-              <strong>Velocidad del Viento:</strong>{" "}
-              {weatherData.Wind.Speed.Metric.Value}{" "}
-              {weatherData.Wind.Speed.Metric.Unit} (
-              {weatherData.Wind.Direction.Localized})
-            </p>
-            <p>
-              <strong>Visibilidad:</strong>{" "}
-              {weatherData.Visibility.Metric.Value}{" "}
-              {weatherData.Visibility.Metric.Unit}
-            </p>
-            <p>
-              <strong>Nivel de Nubosidad:</strong> {weatherData.CloudCover}%
-            </p>
-            <p>
-              <strong>Índice UV:</strong> {weatherData.UVIndex} (
-              {weatherData.UVIndexText})
-            </p>
-            <p>
-              <strong>Presión:</strong> {weatherData.Pressure.Metric.Value}{" "}
-              {weatherData.Pressure.Metric.Unit}
-            </p>
-            <p>
-              <strong>Enlace:</strong>{" "}
-              <a
-                href={weatherData.Link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Más información
-              </a>
-            </p>
+          <div className="p-grid p-dir-col p-md-dir-row p-mt-4">
+            <div className="p-col-12 p-md-6">
+              <Panel header="Información Principal">
+                <div className="p-d-flex p-ai-center">
+                  <img
+                    src={weatherIconUrl(weatherData.WeatherIcon)}
+                    alt={weatherData.WeatherText}
+                    style={{ marginRight: "1em" }}
+                  />
+                  <div>
+                    <p>
+                      <strong>Condición:</strong> {weatherData.WeatherText}
+                    </p>
+                    <p>
+                      <strong>Temperatura:</strong>{" "}
+                      {weatherData.Temperature.Metric.Value}°
+                      {weatherData.Temperature.Metric.Unit}
+                    </p>
+                    <p>
+                      <strong>Temperatura Real:</strong>{" "}
+                      {weatherData.RealFeelTemperature.Metric.Value}°
+                      {weatherData.RealFeelTemperature.Metric.Unit} (
+                      {weatherData.RealFeelTemperature.Metric.Phrase})
+                    </p>
+                  </div>
+                </div>
+              </Panel>
+            </div>
+            <div className="p-col-12 p-md-6">
+              <Panel header="Detalles Adicionales">
+                <div>
+                  <p>
+                    <strong>Humedad Relativa:</strong>{" "}
+                    {weatherData.RelativeHumidity}%
+                  </p>
+                  <p>
+                    <strong>Hora de Observación:</strong>{" "}
+                    {new Date(
+                      weatherData.LocalObservationDateTime
+                    ).toLocaleString()}
+                  </p>
+                  <p>
+                    <strong>Velocidad del Viento:</strong>{" "}
+                    {weatherData.Wind.Speed.Metric.Value}{" "}
+                    {weatherData.Wind.Speed.Metric.Unit} (
+                    {weatherData.Wind.Direction.Localized})
+                  </p>
+                  <p>
+                    <strong>Visibilidad:</strong>{" "}
+                    {weatherData.Visibility.Metric.Value}{" "}
+                    {weatherData.Visibility.Metric.Unit}
+                  </p>
+                  <p>
+                    <strong>Nivel de Nubosidad:</strong>{" "}
+                    {weatherData.CloudCover}%
+                  </p>
+                  <p>
+                    <strong>Índice UV:</strong> {weatherData.UVIndex} (
+                    {weatherData.UVIndexText})
+                  </p>
+                  <p>
+                    <strong>Presión:</strong>{" "}
+                    {weatherData.Pressure.Metric.Value}{" "}
+                    {weatherData.Pressure.Metric.Unit}
+                  </p>
+                  <p>
+                    <strong>Enlace:</strong>{" "}
+                    <a
+                      href={weatherData.Link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Más información
+                    </a>
+                  </p>
+                </div>
+              </Panel>
+            </div>
           </div>
         )}
       </Card>
