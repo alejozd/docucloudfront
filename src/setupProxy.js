4be652109822ce5077a8ad02b076d43b962b5dd0
alejozd <alejozd79@gmail.com>
2024-06-10 21:56:51 -0500

    pero se frego todo

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,31 +1,47 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = function (app) {
+module.exports = function addProxyMiddleware(app) {
+  console.log("Proxy middleware setup");
   app.use(
-    "/api",
+    "/api/phrase",
     createProxyMiddleware({
-      target: "http://dataservice.accuweather.com",
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    "/geo",
-    createProxyMiddleware({
-      target: "http://api.openweathermap.org",
+      target: "https://frasedeldia.azurewebsites.net",
       changeOrigin: true,
       pathRewrite: {
-        "^/geo": "/geo/1.0",
+        "^/api/phrase": "/api/phrase",
+      },
+      onProxyReq: (proxyReq, req, res) => {
+        console.log(
+          `Proxying request to: ${proxyReq.getHeader("host")}${proxyReq.path}`
+        );
       },
     })
   );
-  app.use(
-    "/data",
-    createProxyMiddleware({
-      target: "https://api.openweathermap.org",
-      changeOrigin: true,
-      pathRewrite: {
-        "^/data": "/data/2.5",
-      },
-    })
-  );
+  // app.use(
+  //   "/api",
+  //   createProxyMiddleware({
+  //     target: "http://dataservice.accuweather.com",
+  //     changeOrigin: true,
+  //   })
+  // );
+  // app.use(
+  //   "/geo",
+  //   createProxyMiddleware({
+  //     target: "http://api.openweathermap.org",
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       "^/geo": "/geo/1.0",
+  //     },
+  //   })
+  // );
+  // app.use(
+  //   "/data",
+  //   createProxyMiddleware({
+  //     target: "https://api.openweathermap.org",
+  //     changeOrigin: true,
+  //     pathRewrite: {
+  //       "^/data": "/data/2.5",
+  //     },
+  //   })
+  // );
 };
