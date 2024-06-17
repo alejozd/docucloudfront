5514e9da6857746ca6b26989ffde083e4f576988
Alejandro Zambrano <alejozd79@gmail.com>
2024-06-17 17:13:50 -0500

    no funcion directo

--- src/Home.js
+++ src/Home.js
@@ -9,56 +9,22 @@ const Home = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  // useEffect(() => {
-  //   axios
-  //     .get("/api/phrase")
-  //     // .get("https://frasedeldia.azurewebsites.net/api/phrase")
-  //     .then((response) => {
-  //       console.log("response.data", response.data);
-  //       setPhrase(response.data.phrase);
-  //       setAuthor(response.data.author);
-  //       setLoading(false);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching the phrase of the day", error);
-  //       setError(error.message);
-  //       setLoading(false);
-  //     });
-  // }, []);
-
   useEffect(() => {
-    async function getPhrase() {
-      const result = await fetchPhraseOfDay();
-      if (result) {
-        setPhrase(result);
-      }
-    }
-
-    getPhrase();
+    axios
+      .get("/api/phrase")
+      // .get("https://frasedeldia.azurewebsites.net/api/phrase")
+      .then((response) => {
+        console.log("response.data", response.data);
+        setPhrase(response.data.phrase);
+        setAuthor(response.data.author);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching the phrase of the day", error);
+        setError(error.message);
+        setLoading(false);
+      });
   }, []);
-
-  async function fetchPhraseOfDay() {
-    try {
-      const response = await fetch("https://proverbia.net/frase-del-dia");
-      const html = await response.text();
-
-      // Crear un objeto DOM para manipular el HTML
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-
-      // Obtener el texto de la frase y del autor
-      const statement = doc.querySelector(".bsquote p").innerText.trim();
-      const author = doc
-        .querySelector(".bsquote a")
-        .innerText.trim()
-        .replace("—", "");
-
-      return { phrase: statement, author: author };
-    } catch (error) {
-      console.error("Error al obtener la frase del día:", error);
-      return null;
-    }
-  }
 
   return (
     <div>
