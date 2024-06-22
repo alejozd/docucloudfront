8c9e4741b95397d0f80098ab974b71c430d541ee
alejozd <alejozd79@gmail.com>
2024-06-22 12:16:34 -0500

    el mani

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-<link rel="manifest" href="/manifest.json"></link>;
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
