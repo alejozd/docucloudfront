9175d658b9bcb8c3107e4aea746b56a8d0516751
alejozd <alejozd79@gmail.com>
2025-03-04 01:31:33 -0500

    con clave token

--- src/ProtectedRoute.js
+++ src/ProtectedRoute.js
@@ -1,10 +1,18 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
-const ProtectedRoute = ({ children }) => {
-  const token = sessionStorage.getItem("jwtToken");
+const ProtectedRoute = ({ isAuthenticated, jwtToken, children }) => {
+  const location = useLocation();
 
-  return token ? children : <Navigate to="/login" replace />;
+  if (!isAuthenticated) {
+    console.warn("Token no encontrado. Redirigiendo a /login...");
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  // Pasar el token como prop a los componentes protegidos
+  return React.Children.map(children, (child) =>
+    React.cloneElement(child, { jwtToken })
+  );
 };
 
 export default ProtectedRoute;
