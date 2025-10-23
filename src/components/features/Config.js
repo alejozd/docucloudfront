bc10ea0a2708dd942cffd7395f429c059f4c0239
alejozd <alejozd79@gmail.com>
2025-10-22 23:07:16 -0500

    Cambio de url para el back

--- src/components/features/Config.js
+++ src/components/features/Config.js
@@ -1,7 +1,7 @@
 // Config.js
 const Config = {
-  apiUrl: "https://zetamini.ddns.net", // Url Prod
-  // apiUrl: "http://Localhost:3100", // Url Dev
+  // apiUrl: "https://zetamini.ddns.net", // Url Prod
+  apiUrl: "http://Localhost:3100", // Url Dev
 };
 
 export default Config;
